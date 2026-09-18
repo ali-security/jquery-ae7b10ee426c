4f1b8b110540f4b937b4eaf839b91d3192d2b85d
Seal <info@sealsecurity.io>
2026-09-18 08:43:56 +0000

    CVE-2016-10707

--- test/unit/attributes.js
+++ test/unit/attributes.js
@@ -477,7 +477,9 @@ test( "attr(String, Object)", function() {
 test( "attr - extending the boolean attrHandle", function() {
 	expect( 1 );
 	var called = false,
-		_handle = jQuery.expr.attrHandle.checked || $.noop;
+		origAttrHandleHadChecked = "checked" in jQuery.expr.attrHandle,
+		origAttrHandleChecked = jQuery.expr.attrHandle.checked,
+		_handle = origAttrHandleChecked || $.noop;
 	jQuery.expr.attrHandle.checked = function() {
 		called = true;
 		_handle.apply( this, arguments );
@@ -486,6 +488,12 @@ test( "attr - extending the boolean attrHandle", function() {
 	called = false;
 	jQuery( "input" ).attr( "checked" );
 	ok( called, "The boolean attrHandle does not drop custom attrHandles" );
+
+	if ( origAttrHandleHadChecked ) {
+		jQuery.expr.attrHandle.checked = origAttrHandleChecked;
+	} else {
+		delete jQuery.expr.attrHandle.checked;
+	}
 });
 
 test( "attr(String, Object) - Loaded via XML document", function() {
@@ -1475,4 +1483,46 @@ test( "Insignificant white space returned for $(option).val() (#14858)", functio
 
 	val = jQuery( "<option>  test  </option>" ).val();
 	equal( val.length, 4, "insignificant white-space returned for value" );
+});
+
+test( "non-lowercase boolean attribute getters should not crash", function() {
+	expect( 7 );
+
+	var elem = jQuery( "<input checked required autofocus type='checkbox'>" ),
+		container = jQuery( "<div></div>" ).append( elem, "<input type='checkbox'>" ),
+		mixedCase = {
+			checked: "Checked",
+			required: "requiRed",
+			autofocus: "AUTOFOCUS"
+		};
+
+	// The boolean attrHandle wrapper must remove itself under the lowercased
+	// name, otherwise it keeps calling itself until the stack blows up
+	jQuery.each( mixedCase, function( lowercased, original ) {
+		try {
+			strictEqual( elem.attr( original ), lowercased,
+				"The '" + original + "' attribute getter should return the lowercased name" );
+		} catch ( e ) {
+			ok( false, "The '" + original + "' attribute getter threw: " + e );
+		}
+	});
+
+	// The selector engine passes the name along with its original case
+	jQuery.each( mixedCase, function( lowercased, original ) {
+		try {
+			strictEqual( jQuery.find.attr( elem[ 0 ], original ), lowercased,
+				"jQuery.find.attr should return the lowercased '" + original + "' name" );
+		} catch ( e ) {
+			ok( false, "jQuery.find.attr threw for '" + original + "': " + e );
+		}
+	});
+
+	// ...including when it filters a set through an attribute selector it
+	// evaluates itself instead of handing it to querySelectorAll
+	try {
+		strictEqual( container.find( "input" ).filter( "[Checked]" ).length, 1,
+			"A mixed-case boolean attribute selector should match the checked input" );
+	} catch ( e ) {
+		ok( false, "A mixed-case boolean attribute selector threw: " + e );
+	}
 });
